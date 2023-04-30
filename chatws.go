1aad75658921f29728ca5910022e196b896e126a
Miguel Galicia <migel.gt@outlook.com>
2023-04-30 17:29:05 -0600

    add data model and services definitions

---
+++ chatws.go
@@ -0,0 +1,21 @@
+package chatws
+
+import "io"
+
+// TokenPayload describes a JTW payload data for a client
+type TokenPayload struct {
+	ClientID string
+	Topics   map[string][]string
+}
+
+// AuthService describes the behaviour to perform clients authentication
+type AuthService interface {
+	Authenticate(jwt string) (*TokenPayload, error)
+}
+
+// MessageBroker describes the behavior to perform Pub/Sub operations.
+type MessageBroker interface {
+	Subscribe(topics map[string][]string, clientID string, stop chan struct{}, callback func(msg io.Reader) error)
+	Publish(topic, routeKey string, msg io.Reader) error
+	Close() error
+}
