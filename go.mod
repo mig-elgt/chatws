40b456bb61b9085952d709b121fc6038ac77ec86
Miguel Galicia <migel.gt@outlook.com>
2023-04-30 18:22:09 -0600

    add websocket library to upgrade HTTP connection to WebSocket connection

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/streadway/amqp v1.0.0 // indirect
 )
--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 	"github.com/mig-elgt/chatws"
 )
 
@@ -19,6 +22,29 @@ func New(auth chatws.AuthService, broker chatws.MessageBroker) http.Handler {
 	return r
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 func (h handler) wsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	conn.WriteMessage(1, []byte("hi client!"))
+	for {
+		msgType, p, err := conn.ReadMessage()
+		if err != nil {
+			// Handle close error
+			fmt.Printf("%T, %v", err, err)
+			return
+		}
+		fmt.Printf("got message: %v\n", string(p))
+		if err := conn.WriteMessage(msgType, p); err != nil {
+			log.Println(err)
+		}
+	}
 }
