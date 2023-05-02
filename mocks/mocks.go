e2727e8a3fe2a96c16c559d9843dee11d0731fde
Miguel Galicia <migel.gt@outlook.com>
2023-05-01 19:31:44 -0600

    Add and implement a MessageBroker Mock

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -1,6 +1,10 @@
 package mocks
 
-import "github.com/mig-elgt/chatws"
+import (
+	"io"
+
+	"github.com/mig-elgt/chatws"
+)
 
 type AuthServiceMock struct {
 	AuthenticateFn func(jwt string) (*chatws.TokenPayload, error)
@@ -8,4 +12,21 @@ type AuthServiceMock struct {
 
 func (svc *AuthServiceMock) Authenticate(jwt string) (*chatws.TokenPayload, error) {
 	return svc.AuthenticateFn(jwt)
+}
+
+type MessageBrokerMock struct {
+	SubscribeFn func(topics map[string][]string, clientID string, stop chan struct{}, callback func(msg io.Reader) error) error
+	PublishFn   func(topic string, subTopic string, msg io.Reader) error
+}
+
+func (m *MessageBrokerMock) Subscribe(topics map[string][]string, clientID string, stop chan struct{}, callback func(msg io.Reader) error) error {
+	return m.SubscribeFn(topics, clientID, stop, callback)
+}
+
+func (m *MessageBrokerMock) Publish(topic string, subTopic string, msg io.Reader) error {
+	return m.PublishFn(topic, subTopic, msg)
+}
+
+func (m *MessageBrokerMock) Close() error {
+	return nil
 }
